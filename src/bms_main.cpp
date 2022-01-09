c33dd6a4069a064b648b5d3de69a9afb7929dc97
lolwheel <lolwheel@protonmail.org>
2022-01-08 18:56:25 -0800

    Scaffolding for embedding data files into the sources.
    Some housekeeping too, e.g. VSCode formatter settings.

--- src/bms_main.cpp
+++ src/bms_main.cpp
@@ -1,6 +1,10 @@
 #include <Arduino.h>
+#include <DNSServer.h>
+#include <ESP8266WiFi.h>
+#include <ESPAsyncWebServer.h>
 
 #include "bms_relay.h"
+#include "data.h"
 
 // UART RX is connected to the *BMS* White line
 // UART TX is connected to the *MB* White line
@@ -11,14 +15,38 @@
 
 namespace {
 
+DNSServer dnsServer;
+AsyncWebServer webServer(80);
+
 // Emulate the RS485 B line by bitbanging the inverse
-// of the A line.
+// of the TX A line.
 void IRAM_ATTR txPinRiseInterrupt() { digitalWrite(TX_INVERSE_OUT_PIN, 0); }
 void IRAM_ATTR txPinFallInterrupt() { digitalWrite(TX_INVERSE_OUT_PIN, 1); }
 }  // namespace
 
 BmsRelay relay([]() { return Serial.read(); },
                [](uint8_t b) { return Serial.write(b); });
+
+void setupWebServer() {
+  WiFi.mode(WIFI_AP);
+  char apName[64];
+  sprintf(apName, "OWEnhancer-%04X", ESP.getChipId() & 0xFFFF);
+  WiFi.softAP(apName);
+  dnsServer.start(53, "*", WiFi.softAPIP());  // DNS spoofing.
+  webServer.onNotFound([&](AsyncWebServerRequest *request) {
+    request->redirect("http://" + WiFi.softAPIP().toString() + "/");
+  });
+  webServer.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send_P(200, "text/html", INDEX_HTML, sizeof(INDEX_HTML),
+                    [](const String &var) {
+                      if (var == "OWE_version") {
+                        return "0.0.1";
+                      }
+                      return "UNKNOWN";
+                    });
+  });
+  webServer.begin();
+}
 
 void bms_setup() {
   Serial.begin(115200);
@@ -37,6 +65,10 @@ void bms_setup() {
       [] { digitalWrite(LED_BUILTIN, 1 - digitalRead(LED_BUILTIN)); });
   // An example serial override which defeats BMS pairing:
   // relay.setBMSSerialOverride(123456);
+  setupWebServer();
 }
 
-void bms_loop() { relay.loop(); }
+void bms_loop() {
+  relay.loop();
+  dnsServer.processNextRequest();
+}
