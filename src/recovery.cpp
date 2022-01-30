95c86b46fb24cfb8c88487223a4dae84631ef5f9
lolwheel <lolwheel@protonmail.org>
2022-01-29 20:39:58 -0800

    Various fixes.
     - Remove OTA compression, suspect at bricking things.
     - Fix the current prescaler after measuring it via Ah counter.
     - add support fo reentrant scheduling to TaskQueue.

--- src/recovery.cpp
+++ src/recovery.cpp
@@ -1,11 +1,13 @@
 #include "recovery.h"
 
+#include <ArduinoOTA.h>
 #include <AsyncElegantOTA.h>
 #include <DNSServer.h>
 #include <ESP8266WiFi.h>
 #include <ESPAsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 
+#include "settings.h"
 #include "task_queue.h"
 
 #define SSID_NAME "Owie-recovery"
@@ -19,11 +21,20 @@ void recovery_setup() {
   WiFi.setOutputPower(0);
   WiFi.mode(WIFI_AP);
   WiFi.softAP(SSID_NAME);
+  ArduinoOTA.setHostname("owie");
+  ArduinoOTA.begin(true);
+  ArduinoOTA.onStart([]() {
+    disableFlashPageRotation();
+    saveSettings();
+  });
   dnsServer.start(53, "*", WiFi.softAPIP());  // DNS spoofing.
   webServer.onNotFound([&](AsyncWebServerRequest *request) {
     request->redirect("http://" + WiFi.softAPIP().toString() + "/update");
   });
   AsyncElegantOTA.begin(&webServer);
   webServer.begin();
-  TaskQueue.postRecurringTask([&]() { dnsServer.processNextRequest(); });
+  TaskQueue.postRecurringTask([&]() {
+    dnsServer.processNextRequest();
+    ArduinoOTA.handle();
+  });
 }
