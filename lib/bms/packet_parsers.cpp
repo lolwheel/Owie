f6cc3c7e9860bba744dc2187db8109c616b3115a
Jordan Vlieg <jordanvlieg@gmail.com>
2022-08-08 11:48:47 -0700

    [Lookup table version] Improved battery percentage tracking based off actual energy remaining (#55)
    * update lookup table and refactor code slightly for readability
    
    * Fix small bug in lookup table
    
    * Add small comment

--- lib/bms/packet_parsers.cpp
+++ lib/bms/packet_parsers.cpp
@@ -18,9 +18,9 @@ inline int16_t int16FromNetworkOrder(const void* const p) {
 }
 
 int openCircuitSocFromCellVoltage(int cellVoltageMillivolts) {
-  static constexpr int LOOKUP_TABLE_RANGE_MIN_MV = 2762;
-  static constexpr int LOOKUP_TABLE_RANGE_MAX_MV = 4140;
-  static uint8_t LOOKUP_TABLE[11] = {0, 0, 0, 2, 4, 11, 26, 44, 61, 78, 100};
+  static constexpr int LOOKUP_TABLE_RANGE_MIN_MV = 2700;
+  static constexpr int LOOKUP_TABLE_RANGE_MAX_MV = 4200;
+  static uint8_t LOOKUP_TABLE[31] = {0, 0, 0, 0, 1, 2, 3, 4, 5, 7, 8, 11, 14, 16, 18, 19, 25, 30, 33, 37, 43, 48, 53, 60, 67, 71, 76, 82, 92, 97, 100};
   static constexpr int LOOKUP_TABLE_SIZE = (sizeof(LOOKUP_TABLE)/sizeof(*LOOKUP_TABLE));
   static constexpr int RANGE = LOOKUP_TABLE_RANGE_MAX_MV - LOOKUP_TABLE_RANGE_MIN_MV;
   // (RANGE - 1) upper limit effectively clamps the leftIndex below to (LOOKUP_TABLE_SIZE - 2)
